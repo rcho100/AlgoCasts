5e1fb2821519629e64f4d74d0a2aee666be27eba
Richard Cho <richardcho100@gmail.com>
2020-11-23 22:42:05 -0500

    Reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,29 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// this solution uses an array helper the reduce function. this will most likely impress the interviewer
+function reverse(str) {
+    // debugger
+    return str.split('').reduce((rev, char) => rev = char + rev, '')
+}
 
+// using a for of loop
+// function reverse(str) {
+//     let reversed = ''
+//     for (let char of str) {
+//         reversed = char + reversed
+//     }
+//     return reversed
+// }
+
+// simple solution but interviewers will probably ask for something that doesn't use .reverse()
+// function reverse(str) {
+//     return str.split('').reverse().join('')
+// }
+
+reverse('hello')
 module.exports = reverse;
